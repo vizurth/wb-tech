00aa7eccae73028ec9e983fa29d05ec336748441
vizurth <timrsamara@gmail.com>
2025-08-14 22:00:55 +0400

    [back-end] add main.go logic

--- order-back-end/cmd/main.go
+++ order-back-end/cmd/main.go
@@ -1,1 +1,28 @@
 package cmd
+
+import (
+	"context"
+	"go.uber.org/zap"
+	"vizurth/wildberries-test-l0/order-back-end/internal/config"
+	"vizurth/wildberries-test-l0/order-back-end/internal/logger"
+	"vizurth/wildberries-test-l0/order-back-end/internal/postgres"
+)
+
+func main() {
+	ctx := context.Background()
+
+	cfg, err := config.NewConfig()
+
+	if err != nil {
+		logger.GetLoggerFromCtx(ctx).Fatal(ctx, "config.New error", zap.Error(err))
+	}
+
+	db, err := postgres.New(ctx, cfg.Postgres)
+
+	if err != nil {
+		logger.GetLoggerFromCtx(ctx).Fatal(ctx, "postgres.New error", zap.Error(err))
+	}
+	db.Ping(ctx)
+	defer db.Close()
+
+}
