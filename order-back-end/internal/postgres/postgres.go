eebe6702d27c70f11289931adc7416233d3186fa
vizurth <timrsamara@gmail.com>
2025-08-15 19:52:56 +0400

    [back-end] change postgres

--- order-back-end/internal/postgres/postgres.go
+++ order-back-end/internal/postgres/postgres.go
@@ -6,14 +6,16 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// Config конфиг для postgres
 type Config struct {
 	Host     string `yaml:"host" env:"PG_HOST" envDefault:"localhost"`
 	Port     string `yaml:"port" env:"PG_PORT" envDefault:"5432"`
-	User     string `yaml:"user" env:"PG_USER" envDefault:"postgres"`
+	User     string `yaml:"username" env:"PG_USER" envDefault:"postgres"`
 	Password string `yaml:"password" env:"PG_PASSWORD" envDefault:"postgres"`
 	Database string `yaml:"database" env:"PG_DB" envDefault:"postgres"`
 }
 
+// New создаем подключение к бд
 func New(ctx context.Context, cfg Config) (*pgxpool.Pool, error) {
 	connString := cfg.ConnString()
 
@@ -24,6 +26,7 @@ func New(ctx context.Context, cfg Config) (*pgxpool.Pool, error) {
 	return conn, nil
 }
 
+// ConnString создаем строку для подключения
 func (c *Config) ConnString() string {
 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		c.User,
