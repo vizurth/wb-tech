5ba1b71c61dfd245b5063f8cea366756f6fc8801
vizurth <timrsamara@gmail.com>
2025-08-15 19:53:33 +0400

    [back-end] add serv logic

---
+++ order-back-end/internal/service/service.go
@@ -0,0 +1,36 @@
+package order
+
+import (
+	"context"
+	"fmt"
+	"order-back-end/internal/cache"
+	"order-back-end/internal/model"
+	order "order-back-end/internal/repository"
+)
+
+// OrderService часть слоистой архитектуры
+type OrderService struct {
+	repository *order.OrderRepo
+}
+
+// NewOrderService создаем экземпляр класса
+func NewOrderService(repository *order.OrderRepo) *OrderService {
+	return &OrderService{
+		repository: repository,
+	}
+}
+
+func (s *OrderService) GetOrderFromDB(ctx context.Context, orderID string, cache *cache.Cache) (*model.OrderInfo, error) {
+	fmt.Println("GetOrderFromDB: ", orderID)
+	if cachedOrder, ok := cache.Get(orderID); ok {
+		return &cachedOrder, nil
+	}
+
+	dbOrder, err := s.repository.GetOrderFromDB(ctx, orderID)
+	if err != nil {
+		return nil, fmt.Errorf("GetOrderFromDB: %w", err)
+	}
+
+	cache.Set(orderID, *dbOrder)
+	return dbOrder, nil
+}
