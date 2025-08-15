8b51189b88f8f778fbe71bd74a46f95193a99768
vizurth <timrsamara@gmail.com>
2025-08-15 19:50:56 +0400

    [back-end] change config.go with path

--- order-back-end/internal/config/config.go
+++ order-back-end/internal/config/config.go
@@ -2,14 +2,23 @@ package config
 
 import (
 	"github.com/ilyakaznacheev/cleanenv"
-	"vizurth/wildberries-test-l0/order-back-end/internal/postgres"
+	kfk "order-back-end/internal/kafka/config"
+	"order-back-end/internal/postgres"
 )
 
-type Config struct {
-	Port     string          `yaml:"port" envconfig:"PORT" default:"8080"`
-	Postgres postgres.Config `yaml:"postgres" envconfig:"POSTGRES"`
+// httpConfig структура которая содержит порт для подключения по HTTP
+type httpConfig struct {
+	Port string `yaml:"port" envconfig:"PORT" default:"8081"`
 }
 
+// Config структура содержащая основные параменты в конфиге
+type Config struct {
+	HTTP     httpConfig      `yaml:"http" envconfig:"HTTP"`
+	Postgres postgres.Config `yaml:"postgres" envconfig:"POSTGRES"`
+	Kafka    kfk.Config      `yaml:"kafka" envconfig:"KAFKA"`
+}
+
+// NewConfig создает Config
 func NewConfig() (Config, error) {
 	var config Config
 	if err := cleanenv.ReadConfig("./config/config.yaml", &config); err != nil {
